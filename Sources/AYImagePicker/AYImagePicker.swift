0cfaa4be06372e594ebf2a5f582d1f3e53bd8b52
Amir Yalchi <amir.yalchi@gmail.com>
2023-01-01 15:44:18 -0500

    First commit for AYImagePicker

--- Sources/AYImagePicker/AYImagePicker.swift
+++ Sources/AYImagePicker/AYImagePicker.swift
@@ -1,6 +1,31 @@
-public struct AYImagePicker {
-    public private(set) var text = "Hello, World!"
+import SwiftUI
 
-    public init() {
+public struct AYImagePicker { 
+    
+    public static func openImagePicker(
+        sourceType: UIImagePickerController.SourceType,
+        allowsEditing: Bool = false,
+        completion: @escaping (UIImage?) -> Void
+    ) {
+        let imagePicker = UIImagePickerController()
+        imagePicker.sourceType = sourceType
+        imagePicker.allowsEditing = allowsEditing
+        imagePicker.delegate = AYImagePickerDelegate(completion: completion)
+        UIApplication.shared.keyWindow?.rootViewController?.present(imagePicker, animated: true, completion: nil)
     }
+    
+    public static func openCamera(
+        allowsEditing: Bool = false,
+        completion: @escaping (UIImage?) -> Void
+    ) {
+        openImagePicker(sourceType: .camera, allowsEditing: allowsEditing, completion: completion)
+    }
+    
+    public static func openPhotoLibrary(
+        allowsEditing: Bool = false,
+        completion: @escaping (UIImage?) -> Void
+    ) {
+        openImagePicker(sourceType: .photoLibrary, allowsEditing: allowsEditing, completion: completion)
+    }
+    
 }
---
+++ Sources/AYImagePicker/AYImagePickerDelegate.swift
@@ -0,0 +1,39 @@
+//
+//  AYImagePickerDelegate.swift
+//  
+//
+//  Created by Amir Yalchi on 2023-01-01.
+//
+
+import Foundation
+
+
+import UIKit    
+import SwiftUI
+
+final class AYImagePickerDelegate: NSObject, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+    
+    var completion: (UIImage?) -> Void
+    
+    init(completion: @escaping (UIImage?) -> Void) {
+        self.completion = completion
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let image = info[.editedImage] as? UIImage {
+            completion(image)
+        } else if let image = info[.originalImage] as? UIImage {
+            completion(image)
+        } else {
+            completion(nil)
+        }
+        picker.dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        completion(nil)
+        picker.dismiss(animated: true, completion: nil)
+    }
+    
+}
+
